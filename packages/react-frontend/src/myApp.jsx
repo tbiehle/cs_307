fddaf4429ab1f76189725fbd3351140b18b349fb
Travis B <travis.biehle@gmail.com>
2025-04-03 14:09:56 -0700

    job list app complete

--- packages/react-frontend/src/myApp.jsx
+++ packages/react-frontend/src/myApp.jsx
@@ -12,13 +12,17 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table 
         characterData={characters}
         removeCharacter={removeOneCharacter}  
       />
-      <Form />
+      <Form handleSubmit={updateList}/>
     </div>
   );
 }
